4d66e1e6c72ede370643d850cbdbbd90a410effd
Olivia Lucca Fraser <oluccafraser@tenable.com>
2017-08-24 01:54:19 -0300

    master: backup file directory

--- init.el
+++ init.el
@@ -95,6 +95,15 @@
 (global-linum-mode 1)
 (setq linum-format "%d ")
 
+;; backup files:
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying t    ; Don't delink hardlinks
+      version-control t      ; Use version numbers on backups
+      delete-old-versions t  ; Automatically delete excess backups
+      kept-new-versions 20   ; how many of the newest versions to keep
+      kept-old-versions 5    ; and how many of the old
+      )
+
 ;; Aesthetics
 (menu-bar-mode -1)
 (tool-bar-mode -1)
